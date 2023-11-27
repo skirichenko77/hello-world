38c21a130e8ceecc1c4d1734c7ebdedd6d57480a
Stas <k.stanislav2021@gmail.com>
2023-11-27 17:21:18 +0200

    Додано функцію GetArguments запит двох чисел та їх сума

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('Добрий день, наш герой Котигорошко')
+function getArguments() {
+    var n1 = Number(prompt('Введіть перше число:', '0'));
+    var n2 = Number(prompt('Введіть друге число:', '0'));
+    return { n1: n1, n2: n2 };
+}
+
+function welcome() {
+    var args = getArguments();
+    var sum = args.n1 + args.n2;
+    alert('Привітання! Сума введених чисел: ' + sum);
+}
+
+// Виклик функції welcome()
+welcome();
