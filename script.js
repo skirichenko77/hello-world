4a7616e143a53e5d84b499a7451fbe515e21146a
Stas <k.stanislav2021@gmail.com>
2023-11-30 10:40:53 +0200

    Змінено скріпт

--- script.js
+++ script.js
@@ -12,17 +12,3 @@ function welcome() {
 
 // Виклик функції welcome()
 welcome();
-
-function getArguments() {
-    var n1 = Number(prompt('Введіть перше число:', '0'));
-    var n2 = Number(prompt('Введіть друге число:', '0'));
-    return { n1: n1, n2: n2 };
-}
-
-let numbers = getArguments();
-
-function calcSum(x = 0, y = 0) {
-    return x + y;
-}
-
-alert('Сума введених чисел: ' + calcSum(numbers.n1, numbers.n2));
